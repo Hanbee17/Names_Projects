a30b6c4c2c0740b5bc92f987f4c68f2ab05bc3a6
Hanbee17 <hanbee.cho@sjsu.edu>
2022-05-24 12:07:34 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -294,7 +294,9 @@ class Bubble {
     this.y--
     if (this.y < windowHeight*0.65) {
       this.y = -height;
-    }  
+    // } else if (this.y > windowHeight*0.65) {
+    //   this.y = windowHeight+10
+    // }
   }
   body() {
     fill(0, 119, 182);
